b9d52f53d58ad167c5a02d9bd24b100f4107ab40
ios8043 <ios8043@caelums-Mac-mini.local>
2019-02-09 09:57:42 -0200

    Consumo de webservice

--- ContatosIP67/TemperaturaViewController.swift
+++ ContatosIP67/TemperaturaViewController.swift
@@ -16,7 +16,7 @@ class TemperaturaViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     let URL_BASE = "https://api.openweathermap.org/data/2.5/weather?APPID=2e4ac28e67124ec30b70b8c9771fdf13&units=metric"
-    let URL_IMAGE = "https://openweathermap.org/img/w/"
+    let URL_IMAGE_BASE = "https://openweathermap.org/img/w/"
     
     var contato: Contato?
     
@@ -26,23 +26,64 @@ class TemperaturaViewController: UIViewController {
         // Do any additional setup after loading the view.
         if let contato = self.contato {
             if let endpoint = URL(string: URL_BASE + "&lat=\(contato.latitude ?? 0)&lon=\(contato.longitude ?? 0)") {
-                let session = URLSession(configuration: default)
+                let session = URLSession(configuration: .default)
                 print(endpoint)
                 let task = session.dataTask(with: endpoint) {(data, response, error) in
                     if let httpResponse =  response as? HTTPURLResponse {
-                        if httpResponse.statusConde == 200 {
+                        if httpResponse.statusCode == 200 {
                             do {
                                 if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String:AnyObject] {
                                     let main = json["main"] as! [String:AnyObject]
                                     let weather = json["weather"]![0] as! [String:AnyObject]
-                                    let temp_min = json["temp_min"] as! Double
-                                    let temp_max = json["temp_max"] as! Double
+                                    let temp_min = main["temp_min"] as! Double
+                                    let temp_max = main["temp_max"] as! Double
+                                    let icon = weather["icon"] as! String
+                                    let condicao = weather["main"] as! String
+                                    
+                                    DispatchQueue.main.async {
+                                        print(condicao)
+                                        print(temp_min)
+                                        print(temp_max)
+                                        print(icon)
+                                        
+                                        self.labelCondicaoAtual.text = condicao
+                                        self.labelTemperaturaMinima.text = temp_min.description + "º"
+                                        self.labelTemperaturaMaxima.text = temp_max.description + "º"
+                                        self.pegaImagem(icon)
+                                        
+                                        self.labelCondicaoAtual.isHidden = false
+                                        self.labelTemperaturaMinima.isHidden = false
+                                        self.labelTemperaturaMaxima.isHidden = false
+                                    }
                                 }
+                            } catch let erro as NSError {
+                                print(erro.localizedDescription)
                             }
                         }
                     }
                 }
+                task.resume()
             }
+        }
+    }
+    
+    private func pegaImagem(_ icon:String){
+        if let endpoint = URL(string: URL_IMAGE_BASE + icon + ".png"){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: endpoint){ (data, response, error) in
+                
+                if let httpResponse = response as? HTTPURLResponse {
+                    
+                    if httpResponse.statusCode == 200 {
+                        DispatchQueue.main.async {
+                            print("Exibindo Imagem")
+                            self.imageView.image = UIImage(data: data!)
+                        }
+                    }
+                    
+                }
+            }
+            task.resume()
         }
     }
 
